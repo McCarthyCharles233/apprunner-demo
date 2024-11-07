edf4212d33ee6509aaf5d69215ee266a5938cfa5
McCarthyCharles233 <charlesmccarthy233@gmail.com>
2024-11-07 14:09:46 +0000

    intial commit

---
+++ src/app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, request
+
+# Create an instance of the Flask class
+app = Flask(__name__)
+
+# Define the home route
+@app.route('/')
+def home():
+    return "<h1>Welcome to My Simple Web App</h1><p>Navigate to /greet to see a greeting message!</p>"
+
+# Define a route to display a greeting
+@app.route('/greet', methods=['GET', 'POST'])
+def greet():
+    if request.method == 'POST':
+        name = request.form.get('name')
+        return f"<h1>Hello, {name}!</h1>"
+    return '''
+        <form method="post">
+            <label for="name">Enter your name:</label><br>
+            <input type="text" id="name" name="name"><br><br>
+            <input type="submit" value="Greet Me">
+        </form>
+    '''
+
+# Run the app
+if __name__ == '__main__':
+    app.run(debug=True)
