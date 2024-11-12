cd43c012356f8fe75944bde5b841d702c57e3290
McCarthyCharles233 <charlesmccarthy233@gmail.com>
2024-11-12 21:09:12 +0000

    added bash script to execute push commands that builds and  upload docker images to ECR

--- terraform-apprunner-module/Modules/container-image/ecr-repo/ecr.tf
+++ terraform-apprunner-module/Modules/container-image/ecr-repo/ecr.tf
@@ -1,19 +1,6 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
-
-provider "aws" {
-  region = "us-east-1"
-}
-
-resource "aws_ecr_repository" "my-flask-app" {
-  name                 = "my-flask-app" 
-  image_tag_mutability = "INMMUTABLE"
+resource "aws_ecr_repository" "apprunner" {
+  name                 = "apprunner"
+  image_tag_mutability = "IMMUTABLE"
 
   image_scanning_configuration {
     scan_on_push = true
